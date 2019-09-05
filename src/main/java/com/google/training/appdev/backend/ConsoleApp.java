b984be2fabea1ae2f402cac55bfae4799d7e9795
Denys_Smakovskyi <smakovskiy@ukr.net>
2019-09-05 13:38:18 +0300

    Idea project is added for ignoring

--- src/main/java/com/google/training/appdev/backend/ConsoleApp.java
+++ src/main/java/com/google/training/appdev/backend/ConsoleApp.java
@@ -16,16 +16,13 @@
 package com.google.training.appdev.backend;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.core.ApiService;
 import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import com.google.cloud.pubsub.v1.MessageReceiver;
 import com.google.cloud.pubsub.v1.Subscriber;
 import com.google.cloud.pubsub.v1.SubscriptionAdminClient;
 import com.google.common.util.concurrent.MoreExecutors;
-import com.google.pubsub.v1.PubsubMessage;
-import com.google.pubsub.v1.PushConfig;
-import com.google.pubsub.v1.SubscriptionName;
-import com.google.pubsub.v1.TopicName;
-
+import com.google.pubsub.v1.*;
 
 
 import com.google.training.appdev.services.gcp.domain.Feedback;
@@ -42,93 +39,25 @@ public class ConsoleApp {
 
 
     // Notice that the code to create the topic is the same as in the publisher
-    TopicName topic = TopicName.create(projectId, "feedback");
+    TopicName topicName = TopicName.create(projectId, "feedback");
 
-    // TODO: Create the languageService
 
+    LanguageService languageService = LanguageService.create();
+
+    SpannerService spannerService = SpannerService.create();
+
+    SubscriptionName subscriptionName = SubscriptionName.create(projectId,
+                                                                "worker1-subscription");
+
+    try(SubscriptionAdminClient subscriptionAdminClient = SubscriptionAdminClient.create()){
+        Subscription subscription = subscriptionAdminClient.createSubscription(subscriptionName, topicName, PushConfig.getDefaultInstance(), 0);
+    }
     
 
-    // END TODO
-
-    // TODO: Create the spannerService
-
-    
-
-    // END TODO
-
-    // TODO: Create the Pub/Sub subscription name
-
-    
-
-    // END TODO
-    
-    // TODO: Create the subscriptionAdminClient
-
-    
-
-      // TODO: create the Pub/Sub subscription using the subscription name and topic
-
-      
-
-      // END TODO
-      
-    
-
-    // END TODO
 
     // The message receiver processes Pub/Sub subscription messages
-    MessageReceiver receiver = new MessageReceiver() {
-        // Override the receiveMessage(...) method
-        @Override
-        public void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
-            // TODO: Extract the message data as a JSON String
-
-            
-
-            // END TODO
-
-            // TODO: Ack the message
-
-            
-
-            // END TODO
-
-            try {
-                // Object mapper deserializes the JSON String
-                ObjectMapper mapper = new ObjectMapper();
-
-                // TODO: Deserialize the JSON String representing the feedback
-
-                
-
-                // END TODO
-
-                // TODO: Use the Natural Language API to analyze sentiment
-
-                
-
-                // END TODO
-
-                // TODO: Set the feedback object sentiment score
-
-                
-
-                // END TODO
-
-                // TODO: Insert the feedback into Cloud Spanner
-
-                
-
-                // END TODO
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    };
-
-    // TODO: Declare a subscriber
-
+    MessageReceiver receiver = getMessageReceiver(languageService, spannerService);
+    Subscriber subscriber = null;
     
 
     // END TODO
@@ -137,50 +66,61 @@ public class ConsoleApp {
 
         // TODO: Initialize the subscriber using its default builder
         // with a subscription and receiver
+        subscriber = Subscriber.defaultBuilder(subscriptionName, receiver).build();
+        subscriber.addListener( new Subscriber.Listener(){
+            @Override
+            public void failed(ApiService.State from, Throwable failure) {
+                super.failed(from, failure);
+            }
+        },MoreExecutors.directExecutor() );
 
-        
-
-        // END TODO
-
-        // TODO: Add a listener to the subscriber
-
-        
-        
-
-
-
-
-        // END TODO
-
-        // TODO: Start subscribing
-
-        
-        
-
-        // END TODO
-
+        subscriber.startAsync().awaitRunning();
         System.out.println("Started. Press any key to quit and remove subscription");
-
         System.in.read();
 
     } finally {
-        
-        // TODO: Stop subscribing
-
-        
-
-        // END TODO
-        
-
-        // TODO: Delete the subscription
-
-        
-        
+        if (subscriber != null) {
+            subscriber.stopAsync().awaitTerminated();
+        }
 
 
+        try (SubscriptionAdminClient
+                     subscriptionAdminClient =
+                     SubscriptionAdminClient.create()) {
 
-        // END TODO
+            subscriptionAdminClient.deleteSubscription(
+                    subscriptionName);
+        }
+
     }
+    }
+
+    private static MessageReceiver getMessageReceiver(LanguageService languageService, SpannerService spannerService) {
+        return new MessageReceiver() {
+
+            @Override
+            public void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
+                // TODO: Extract the message data as a JSON String
+                String jsonMessage = message.getData().toStringUtf8();
+                consumer.ack();
+                try {
+                    // Object mapper deserializes the JSON String
+                    ObjectMapper mapper = new ObjectMapper();
+                    Feedback feedback = mapper.readValue(jsonMessage, Feedback.class);
+                    float sentiment = languageService.analyzeSentiment(feedback.getFeedback());
+                    feedback.setSentimentScore(sentiment);
+
+                    // TODO: Insert the feedback into Cloud Spanner
+                    spannerService.insertFeedback(feedback);
+
+
+                    // END TODO
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
 }
